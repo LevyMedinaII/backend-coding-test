9068ca9f07d81335cc3f2e158431d6d8a4a25ab3
LevyMedinaII <levymedina3@gmail.com>
2020-10-07 01:15:25 +0800

    add load testing with artillery

--- index.js
+++ index.js
@@ -14,10 +14,16 @@ db.all = util.promisify(db.all);
 const buildSchemas = require('./src/schemas');
 const logger = require('./src/utils/logger');
 
+const compression = require('compression');
+
 db.serialize(() => {
     buildSchemas(db);
 
     const app = require('./src/app')(db);
+
+    if (process.env.NODE_ENV === 'production') {
+        app.use(compression());
+    }
 
     app.listen(port, () => logger.info(`App started and listening on port ${port}`));
 });
